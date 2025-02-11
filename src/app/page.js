0891627051253aab5158190bf438ceefece33a7e
TaniaTenorio <taniatenorio@hotmail.com>
2025-02-10 21:29:02 -0600

    initial template

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,78 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import data from "./database.json"
+
+const WineCard = ({ name, house, variety, year, color, country, region, price }) => {
+  return (
+  <div className={styles.card}>
+    <div style={{textAlign: 'center', marginBottom: '16px'}}>
+      <Image 
+        src={'/assets/wine-clipart.png'}
+        width={37}
+        height={150}
+        alt="wine-bottle"
+      />
+    </div>
+    <p>{name}</p>
+    {/* <p>{variety}</p>
+    <p>{year}</p>
+    <p>{color}</p>
+    <p>{country}</p>
+    <p>{region}</p> */}
+    <p>${price} MXN</p>
+  </div>
+  )
+}
 
 export default function Home() {
+  console.log(data);
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <header style={{backgroundColor: '#c69e0b', textAlign:'center', fontSize: 32}}>
+      Tirando Vino
+    </header>
+        <main>
+        <div className={styles.navbar}>
+              <p>Tintos</p>
+              <p>Blancos</p>
+              <p>Espumosos</p>
+              <p>Rosados</p>
+              <p>Por País</p>
+              <p>Por Marca</p>
+              <p></p>
+            </div>
+          <article style={{padding: '0 px 80px'}}>
+            <p>
+              ¿Quienes somos?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+            </p>
+          </article>
+          <br />
+          <article style={{padding: '0px 80px'}}>
+            <section>
+              <div className={styles.list}>
+                {data.map((el, index) => (
+                  <WineCard
+                    key={index} 
+                    name={el.name}
+                    house={el.house}
+                    variety={el.variety}
+                    year={el.year}
+                    color={el.color}
+                    country={el.country}
+                    region={el.region}
+                    price={el.price}
+                  />
+                ))}
+              </div>
+            </section>
+          </article>
+        </main>
+        <footer>
+          Contáctanos
+        </footer>
     </div>
   );
 }

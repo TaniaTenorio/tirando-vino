cf92ad710d52e564b2698996abd6110ea02806cb
TaniaTenorio <taniatenorio@hotmail.com>
2025-02-11 21:05:31 -0600

    use MUI components

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,44 @@
+'use client'
+
 import Image from "next/image";
 import styles from "./page.module.css";
 import data from "./database.json"
+import { AppBar, Button, Toolbar, IconButton, Typography, Card, CardContent, CardActions } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { amber } from '@mui/material/colors';
+import Grid from '@mui/material/Grid2';
+
+const primary = amber[500]
 
 const WineCard = ({ name, house, variety, year, color, country, region, price }) => {
   return (
-  <div className={styles.card}>
-    <div style={{textAlign: 'center', marginBottom: '16px'}}>
-      <Image 
-        src={'/assets/wine-clipart.png'}
-        width={37}
-        height={150}
-        alt="wine-bottle"
-      />
-    </div>
-    <p>{name}</p>
-    {/* <p>{variety}</p>
-    <p>{year}</p>
-    <p>{color}</p>
-    <p>{country}</p>
-    <p>{region}</p> */}
-    <p>${price} MXN</p>
-  </div>
+  <Card 
+  className={styles.card}
+  >
+    <CardContent>
+      <div style={{textAlign: 'center', marginBottom: '16px'}}>
+        <Image 
+          src={'/assets/wine-clipart.png'}
+          width={37}
+          height={150}
+          alt="wine-bottle"
+        />
+      </div>
+      <Typography>{name}</Typography>
+      {/* <p>{variety}</p>
+      <p>{year}</p>
+      <p>{color}</p>
+      <p>{country}</p>
+      <p>{region}</p> */}
+      <Typography>${price} MXN</Typography>
+      <CardActions>
+        <Button size='small'>
+          Agregar al carrito
+        </Button>
+      </CardActions>
+
+    </CardContent>
+  </Card>
   )
 }
 
@@ -28,9 +46,19 @@ export default function Home() {
   console.log(data);
   return (
     <div className={styles.page}>
-    <header style={{backgroundColor: '#c69e0b', textAlign:'center', fontSize: 32}}>
+    {/* <header style={{backgroundColor: '#c69e0b', textAlign:'center', fontSize: 32}}>
       Tirando Vino
-    </header>
+    </header> */}
+    <AppBar position="static" style={{backgroundColor: '#c69e0b'}}>
+      <Toolbar variant="dense">
+        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" color="inherit" component="div">
+          Logo Tirando Vino
+        </Typography>
+      </Toolbar>
+    </AppBar>
         <main>
         <div className={styles.navbar}>
               <p>Tintos</p>
@@ -41,32 +69,37 @@ export default function Home() {
               <p>Por Marca</p>
               <p></p>
             </div>
-          <article style={{padding: '0 px 80px'}}>
-            <p>
-              ¿Quienes somos?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-            </p>
+          <article className={styles.infoContent}>
+            
+              <p className={styles.content}>
+                ¿Quienes somos?
+              </p>
+              <p className={styles.content}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+              </p>
+
+            
           </article>
           <br />
           <article style={{padding: '0px 80px'}}>
             <section>
-              <div className={styles.list}>
+              <Grid container spacing={2} style={{justifyContent: 'center'}}>
                 {data.map((el, index) => (
-                  <WineCard
-                    key={index} 
-                    name={el.name}
-                    house={el.house}
-                    variety={el.variety}
-                    year={el.year}
-                    color={el.color}
-                    country={el.country}
-                    region={el.region}
-                    price={el.price}
-                  />
+                  <Grid key={index} >
+                    <WineCard
+                      
+                      name={el.name}
+                      house={el.house}
+                      variety={el.variety}
+                      year={el.year}
+                      color={el.color}
+                      country={el.country}
+                      region={el.region}
+                      price={el.price}
+                    />
+                  </Grid>
                 ))}
-              </div>
+              </Grid>
             </section>
           </article>
         </main>

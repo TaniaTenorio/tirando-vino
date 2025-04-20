00b8399aa84859da619caced1cef11d3bf32cb2d
TaniaTenorio <taniatenorio@hotmail.com>
2025-04-20 01:11:15 -0600

    update metadata

--- src/theme.js
+++ src/theme.js
@@ -1,18 +1,9 @@
 'use client';
 import { createTheme } from '@mui/material/styles';
-import { Shadows_Into_Light } from "next/font/google";
-
-
-const shadowsIntoLight = Shadows_Into_Light({
-    variable: "--font-shadows-into-light",
-    weight: "400",
-    subsets: ["latin"],
-    display: "swap"
-  })
 
 const theme = createTheme({
   typography: {
-    fontFamily: shadowsIntoLight.style.fontFamily,
+    fontFamily: "var(--font-shadows-into-light)",
   },
 });
 

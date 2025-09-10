9d019e1c6d4af9f38e93b1cf7a7db8799ac9b0b8
TaniaTenorio <taniatenorio@hotmail.com>
2025-09-09 22:08:18 -0600

    set eslint config for jsx files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,30 @@
 import config from "eslint-config-xo";
+import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    files: ["**/*.js", "**/*.jsx"],
+    plugins: {
+      react: pluginReact,
+    },
     rules: {
+      ...pluginReact.configs.recommended.rules,
       camelcase: ["error", { properties: "never" }],
+    },
+    settings: {
+      react: {
+        version: "detect", // Automatically detect the React version
+      },
     },
   },
 ]);

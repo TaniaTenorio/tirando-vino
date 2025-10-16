9321672e7280c967adf71d2a9209ecebc7ee2a82
TaniaTenorio <taniatenorio@hotmail.com>
2025-10-15 21:19:53 -0600

    update theme palette

--- src/theme.js
+++ src/theme.js
@@ -5,6 +5,26 @@ const theme = createTheme({
   typography: {
     fontFamily: "var(--font-shadows-into-light)",
   },
+  palette: {
+    primary: {
+      main: "#c69e0b",
+    },
+    secondary: {
+      main: "#c1bdbd",
+    },
+    warning: {
+      main: "#ff9800",
+    },
+    info: {
+      main: "#2196f3",
+    },
+    success: {
+      main: "#4caf50",
+    },
+    error: {
+      main: "#f44336",
+    },
+  },
 });
 
 export default theme;

a3e7e8642763a4f37171b1fa1d27b12b1a2c7697
TaniaTenorio <taniatenorio@hotmail.com>
2026-03-06 16:08:37 -0600

    add public routes

---
+++ src/app/admin/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AdminPage = () => {
+  return (
+    <>
+      <h1>Admin Page</h1>
+      <p>Only accessible to authenticated users.</p>
+    </>
+  );
+};
+
+export default AdminPage;
